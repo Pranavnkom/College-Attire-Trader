5804d9baba9320958fc2f7832b5247cfcf826981
Pranav Kommabathula <pranav2000jan1@gmail.com>
2018-07-23 11:47:08 -0400

    1147

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ jinja_current_directory = jinja2.Environment(
     extensions = ['jinja2.ext.autoescape'],
     autoescape = True)
 
+current_account = {}
+
 class LoginPage(webapp2.RequestHandler):
     def get(self):
         login_template = \
@@ -15,8 +17,14 @@ class LoginPage(webapp2.RequestHandler):
         self.response.write(login_template.render())
 
     def post(self):
-    if self.request.get("deposit_btn") == "Deposit":
-
+        email = self.request.get("email")
+        password =  self.request.get("password")
+        if self.request.get("login_btn") == "Login":
+            logged = Accounts.query(Accounts.email == email).get()
+            current_account = {"logged":logged}
+            self.redirect('/welcome')
+        else:
+            self.redirect('/creation')
 
 class CreationPage(webapp2.RequestHandler):
     def get(self):
@@ -27,21 +35,21 @@ class CreationPage(webapp2.RequestHandler):
     def post(self):
         if self.request.get("create_btn") == "Submit":
             account = Accounts(email = "email", password = "password", mailing_address = "mailing_address", first_name = "first_name", last_name = "last_name")
-<<<<<<< HEAD
             account.put()
             self.redirect('/')
         else:
             self.redirect('/')
-=======
 
-
-
->>>>>>> 8cf8023b79058d2d4807a628fae9d35f2386064a
+class WelcomePage(webapp2.RequestHandler):
+    def get(self):
+        welcome_template = \
+                jinja_current_directory.get_template('templates/welcome.html')
+        self.response.write(welcome_template.render(current_account))
 
 
 app = webapp2.WSGIApplication([
     ('/', LoginPage),
     ('/creation', CreationPage)
-    
+    ('/welcome', WelcomePage)
 
 ], debug=True)
