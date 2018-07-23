5adad9e0d5df219d81d89abe327a93805a91848d
Pranav Kommabathula <pranav2000jan1@gmail.com>
2018-07-23 14:42:20 -0400

    242

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ jinja_current_directory = jinja2.Environment(
 
 current_account = {}
 
-def id_generator(size=20, chars=string.ascii_uppercase + string.digits):
+def id_generator(size=90, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 class LoginPage(webapp2.RequestHandler):
@@ -25,7 +25,7 @@ class LoginPage(webapp2.RequestHandler):
         email = self.request.get("email")
         logged = Accounts.query(Accounts.email == email).get()
         if self.request.get("login_btn") == "Login" and logged.password == self.request.get("password"):
-            self.redirect('/welcome?current_user=' + logged.token)
+            self.redirect('/welcome?current_user=' + logged.tokens)
         else:
             self.redirect('/creation')
 
@@ -44,7 +44,7 @@ class CreationPage(webapp2.RequestHandler):
             first_name =  self.request.get("first_name")
             last_name =  self.request.get("last_name")
             token = id_generator()
-            account = Accounts(email = email, password = password, mailing_address = mailing_address, first_name = first_name, last_name = last_name, token = token)
+            account = Accounts(email = email, password = password, mailing_address = mailing_address, first_name = first_name, last_name = last_name, tokens = token)
             account.put()
             self.redirect('/')
         else:
@@ -55,7 +55,7 @@ class WelcomePage(webapp2.RequestHandler):
         welcome_template = \
                 jinja_current_directory.get_template('templates/welcome.html')
         token = self.request.get("current_user")
-        logged = Accounts.query(Accounts.token == token).get()
+        logged = Accounts.query(Accounts.tokens == token).get()
         current_account = {"logged":logged}
         self.response.write(welcome_template.render(current_account))
 
